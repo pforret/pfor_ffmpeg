06a07b8e969d72ef5dbd0d7b0e9efe2998f20c50
Peter Forret <peter@forret.com>
2013-10-16 13:51:09 +0200

    Video filenames / side to side preview

--- thermography/test1.cmd
+++ thermography/test1.cmd
@@ -1,8 +1,12 @@
 @echo off
 setlocal
 
-set SRC=../videos/FoscamIR_test.mp4
+set SRC=../videos/infrared_bw_sd.mp4
 
-set CURVES=curves=r='0.45/0 0.55/1':g='.25/1 .75/1 .9/0 1/1':b='0/1 .3/0 .9/0 1/1'
+::
+set CURVES=curves=r='0.4/0 0.6/1':g='.25/1 .75/.5 .9/0 1/1':b='0/1 .25/0 .75/0 1/1'
 
-ffplay %SRC% -vf "histeq,%CURVES%"
+
+:: start both windows next to each other so you can compare
+start ffplay %SRC%
+start ffplay %SRC% -vf "histeq,%CURVES%"
